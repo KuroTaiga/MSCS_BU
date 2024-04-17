8f3cf9e2f292387a87b89ebd305fe37d475ef95a
Jiankun (Bob) Dong <doctorbobdong0107@gmail.com>
2024-04-17 18:20:38 -0400

    periodic updates

--- CS767/project/train_text_to_image.py
+++ CS767/project/train_text_to_image.py
@@ -14,6 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# modified by Jiankun Dong to include cg and sgd for optimizer options
+# April 2024
 import argparse
 import logging
 import math
@@ -364,6 +366,10 @@ def parse_args():
     parser.add_argument(
         "--use_8bit_adam", action="store_true", help="Whether or not to use 8-bit Adam from bitsandbytes."
     )
+    # adding the option to use cg optimizer
+    parser.add_argument("--use_cg", action="store_true",help="Whether or not to use conjugated gradient descent optimizer")
+    # adding the option to use SGD optimizer
+    parser.add_argument("--use_sgd", action="store_true",help="Whether or not to use SGD optimizer")
     parser.add_argument(
         "--allow_tf32",
         action="store_true",
@@ -393,12 +399,9 @@ def parse_args():
     )
     parser.add_argument("--adam_beta1", type=float, default=0.9, help="The beta1 parameter for the Adam optimizer.")
     parser.add_argument("--adam_beta2", type=float, default=0.999, help="The beta2 parameter for the Adam optimizer.")
-    parser.add_argument("--adam_weight_decay", type=float, default=1e-2, help="Weight decay to use.")
+    parser.add_argument("--adam_weight_decay", type=float, default=1e-2, help="Weight decay to use for adam.")
     parser.add_argument("--adam_epsilon", type=float, default=1e-08, help="Epsilon value for the Adam optimizer")
-    # adding the option to use cg optimizer
-    parser.add_argument("--use_cg", action="store_true",help="Whether or not to use conjugated gradient descent optimizer")
-    # adding the option to use SGD optimizer
-    parser.add_argument("--use_sgd", action="store_true",help="Whether or not to use SGD optimizer")
+    parser.add_argument("--sgd_weight_decay", type=float, default=1e-2, help="Weight decay to use for sgd.")
     parser.add_argument("--max_grad_norm", default=1.0, type=float, help="Max gradient norm.")
     parser.add_argument("--push_to_hub", action="store_true", help="Whether or not to push the model to the Hub.")
     parser.add_argument("--hub_token", type=str, default=None, help="The token to use to push to the Model Hub.")
@@ -680,6 +683,12 @@ def main():
     # Initialize the optimizer
     if args.use_sgd:
         optimizer_cls = torch.optim.sgd
+        optimizer = optimizer_cls(
+            unet.parameters(),
+            lr=args.learning_rate,
+            weight_decay=args.sgd_weight_decay,
+        )
+
     elif args.use_cg:
         try:
             import cg_opti as cg
@@ -687,7 +696,11 @@ def main():
             raise ImportError(
                 "Please double check the local implementation for conjugated gradient descent method implementation"
             )
-        optimizer_cls = cg.cg_opti
+        optimizer_cls = cg.CGD
+        optimizer = optimizer_cls(
+            unet.parameters(),
+            lr=args.learning_rate,
+        )
     else:
         if args.use_8bit_adam:
             try:
